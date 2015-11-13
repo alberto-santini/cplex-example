6722c74948a2e7197ae69cdefdbf362507cd3a05
Alberto Santini <santini.alberto@gmail.com>
2015-11-13 13:23:33 +0100

    Converted some leftover tab into spaces

--- src/graph.cpp
+++ src/graph.cpp
@@ -10,26 +10,26 @@ namespace tsp_example {
   }
   
   void Graph::generate_random_euclidean_cost_matrix(int square_size) {
-  	std::random_device rd;
-  	std::mt19937 mt(rd());
-  	std::uniform_int_distribution<int> dist(-square_size, square_size);
+      std::random_device rd;
+      std::mt19937 mt(rd());
+      std::uniform_int_distribution<int> dist(-square_size, square_size);
 
-  	std::vector<double> x_coord(n_nodes);
-  	std::vector<double> y_coord(n_nodes);
+      std::vector<double> x_coord(n_nodes);
+      std::vector<double> y_coord(n_nodes);
 
-  	std::generate(x_coord.begin(), x_coord.end(), [&] () { return dist(mt); });
-  	std::generate(y_coord.begin(), y_coord.end(), [&] () { return dist(mt); });
+      std::generate(x_coord.begin(), x_coord.end(), [&] () { return dist(mt); });
+      std::generate(y_coord.begin(), y_coord.end(), [&] () { return dist(mt); });
 
     costs = cost_matrix_type(n_nodes, cost_row_type(n_nodes));
 
-  	for(auto i = 0u; i < costs.size(); ++i) {
+      for(auto i = 0u; i < costs.size(); ++i) {
       costs[i][i] = square_size * 2;
-  		for(auto j = i + 1; j < costs[i].size(); ++j) {
+          for(auto j = i + 1; j < costs[i].size(); ++j) {
         auto dist = std::sqrt(std::pow(x_coord[i] - x_coord[j], 2.0) + std::pow(y_coord[i] - y_coord[j], 2.0));
-				costs[i][j] = dist;
+                costs[i][j] = dist;
         costs[j][i] = dist;
-  		}
-  	}
+          }
+      }
   }
   
   std::ostream& Graph::print(std::ostream& out) const {
